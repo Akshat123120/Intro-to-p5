551e1934d82cba74ad99c0d35aceb171b3e0b6c8
Akshat123120 <74185066+Akshat123120@users.noreply.github.com>
2021-11-11 18:30:08 +0530

    Add files via upload

---
+++ Intro-To-p5/main.js
@@ -0,0 +1,21 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(640,480);
+    canvas.position(110,250);
+    video = createCapture(VIDEO);
+    video.hide();
+    Tint_color = "";
+}
+function draw(){
+    image(video,0,0,640,480);
+    tint(Tint_color);
+}
+function take_snapshot(){
+    save('tint.png');
+}
+function filter_tint(){
+    Tint_color = document.getElementById("colour_name").value;
+}
